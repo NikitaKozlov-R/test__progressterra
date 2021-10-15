631b666f939cc708cabe9d1ad956f91e814f2a58
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-16 00:23:38 +0300

    create big-blur-circle component

---
+++ src/assets/img/graphics/big-blur-circles.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg width="665" height="510" viewBox="0 0 665 510" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_f_4:6)">
+      <circle cx="255" cy="255" r="155" fill="#B97A95" fill-opacity="0.5" />
+    </g>
+    <g filter="url(#filter1_f_4:6)">
+      <circle cx="410" cy="255" r="155" fill="#2D62D6" fill-opacity="0.5" />
+    </g>
+    <defs>
+      <filter
+        id="filter0_f_4:6"
+        x="0"
+        y="0"
+        width="510"
+        height="510"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_4:6" />
+      </filter>
+      <filter
+        id="filter1_f_4:6"
+        x="155"
+        y="0"
+        width="510"
+        height="510"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_4:6" />
+      </filter>
+    </defs>
+  </svg>
+</template>
