120ad7e9fb81d90c072a95bf3b454bb77e6433f3
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-15 22:14:18 +0300

    create s-about component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <s-intro />
+  <s-about />
 </template>
 
 <script>
 import SIntro from '@/components/root/s-intro.vue'
+import SAbout from '@/components/root/s-about.vue'
 
 export default {
   name: 'App',
   components: {
     SIntro,
+    SAbout,
   },
 }
 </script>
