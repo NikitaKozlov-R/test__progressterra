df2bb357bce99d09d311e3bc85d69a51f95df9ce
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-16 00:24:32 +0300

    create s-about component

--- src/components/root/s-about.vue
+++ src/components/root/s-about.vue
@@ -1,5 +1,7 @@
 <template>
-  <s-substrate></s-substrate>
+  <s-substrate>
+    <div class="s-about"></div>
+  </s-substrate>
 </template>
 
 <script>
@@ -12,4 +14,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.test {
+  height: 601px;
+}
+</style>
