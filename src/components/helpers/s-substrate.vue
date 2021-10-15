4f38ad65d1a3156b4e9cb23dd379002b2adb2df4
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-16 00:23:55 +0300

    create s-substrate component

---
+++ src/components/helpers/s-substrate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="s-substrate">
+    <slot />
+    <div class="s-substrate__circles">
+      <big-blur-circles />
+    </div>
+  </div>
+</template>
+
+<script>
+import BigBlurCircles from '@/assets/img/graphics/big-blur-circles.vue'
+
+export default {
+  components: {
+    BigBlurCircles,
+  },
+}
+</script>
+
+<style>
+.s-substrate {
+  width: 100%;
+  background-color: var(--COLOR-BACKGROUND);
+  position: relative;
+}
+.s-substrate__circles {
+  position: absolute;
+  top: 70px;
+  left: 300px;
+}
+</style>
