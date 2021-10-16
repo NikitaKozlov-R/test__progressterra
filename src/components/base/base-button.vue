50d41d818259f80a6c101e94e0f0886b81d47da8
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-17 02:10:53 +0300

    start markup of s-other component

---
+++ src/components/base/base-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="base-button" :style="{ width: width }">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      required: false,
+      default: '414px',
+    },
+  },
+}
+</script>
+
+<style>
+.base-button {
+  height: 87px;
+  font-size: 25px;
+  line-height: 30px;
+  font-weight: 300;
+  color: #fff;
+  border-radius: 20px;
+  background: var(--gradient-primary);
+  border: none;
+  outline: none;
+}
+</style>
--- src/components/root/s-other.vue
+++ src/components/root/s-other.vue
@@ -4,19 +4,38 @@
       <div class="base-heading__wrap">
         <base-heading color="color-primary">Другие сервисы облачной платформы лояльности iProBonus</base-heading>
       </div>
-      <s-substrate>
-        <div class="s-other__item"></div>
+      <s-substrate top="-20%" left="80%" :mode="true" radius="19px">
+        <div class="s-other__item">
+          <div class="s-other__article">
+            <h3 class="s-other__heading">Cгораемые рейтинги</h3>
+            <p class="s-other__chapter">
+              С этим сервисом вы сможете составлять и присваивать рейтинги вашим клиентам или пользователям из различных
+              источников и назначать время жизни для каждого начисления рейтинга. Поощрайте ваших пользователей или
+              клиентов за стремление повысить рейтинг и получайте клентов на всю жизнь!
+            </p>
+            <base-button width="414px">Попробовать бесплатно</base-button>
+          </div>
+          <div class="s-other__logo">
+            <ratings-logo />
+          </div>
+        </div>
       </s-substrate>
     </div>
   </div>
 </template>
 
 <script>
+import BaseHeading from '@/components/base/base-heading.vue'
 import SSubstrate from '@/components/helpers/s-substrate.vue'
+import BaseButton from '@/components/base/base-button.vue'
+import RatingsLogo from '../../assets/img/logos/ratings-logo.vue'
 
 export default {
   components: {
+    BaseHeading,
     SSubstrate,
+    BaseButton,
+    RatingsLogo,
   },
 }
 </script>
