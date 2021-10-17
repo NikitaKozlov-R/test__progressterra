df9dd67f81e754f6f781b05a78d79bf4402269c6
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-17 16:38:06 +0300

    take out header to component

---
+++ src/components/base/base-header.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="s-header">
+    <div class="s-header__logo">
+      <s-header-logo />
+    </div>
+    <div class="s-header__menu">
+      <a href="#" class="s-header__link">О сервисе</a>
+      <a href="#" class="s-header__link">Преимущества</a>
+      <a href="#" class="s-header__link">Как использовать</a>
+      <a href="#" class="s-header__link">IProBonus</a>
+      <a href="#" class="s-header__link">Другие сервисы</a>
+    </div>
+  </header>
+</template>
+
+<script>
+import SHeaderLogo from '@/assets/img/logos/s-header-logo.vue'
+
+export default {
+  components: {
+    SHeaderLogo,
+  },
+}
+</script>
+
+<style>
+.s-header {
+  height: 109px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.s-header__logo {
+  margin: 6px 0 0 45px;
+}
+.s-header__menu {
+  display: flex;
+  gap: 31px;
+  margin-right: 50px;
+}
+.s-header__link {
+  font-size: 18px;
+  font-weight: 300;
+  color: var(--color-primary);
+  text-decoration: none;
+  outline: none;
+  border: none;
+}
+.s-header__link:focus {
+  outline: none;
+}
+.s-header__link::-moz-focus-inner {
+  border: 0;
+}
+</style>
--- src/components/root/s-intro.vue
+++ src/components/root/s-intro.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="s-intro">
-    <header class="s-header">
-      <div class="s-header__logo">
-        <s-header-logo />
-      </div>
-      <div class="s-header__menu">
-        <a href="#" class="s-header__link">О сервисе</a>
-        <a href="#" class="s-header__link">Преимущества</a>
-        <a href="#" class="s-header__link">Как использовать</a>
-        <a href="#" class="s-header__link">IProBonus</a>
-        <a href="#" class="s-header__link">Другие сервисы</a>
-      </div>
-    </header>
+    <base-header />
     <div class="wrapper">
       <div class="s-main">
         <div class="s-main__logo">
@@ -29,13 +18,13 @@
 </template>
 
 <script>
-import SHeaderLogo from '@/assets/img/logos/s-header-logo.vue'
 import SMainLogo from '@/assets/img/logos/s-main-logo.vue'
+import BaseHeader from '@/components/base/base-header.vue'
 
 export default {
   components: {
-    SHeaderLogo,
     SMainLogo,
+    BaseHeader,
   },
 }
 </script>
@@ -46,34 +35,6 @@ export default {
   background-image: url('~@/assets/img/photos/s-intro-background.jpg');
   background-position: right;
   background-repeat: no-repeat;
-}
-.s-header {
-  height: 109px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.s-header__logo {
-  margin: 6px 0 0 45px;
-}
-.s-header__menu {
-  display: flex;
-  gap: 31px;
-  margin-right: 50px;
-}
-.s-header__link {
-  font-size: 18px;
-  font-weight: 300;
-  color: var(--color-primary);
-  text-decoration: none;
-  outline: none;
-  border: none;
-}
-.s-header__link:focus {
-  outline: none;
-}
-.s-header__link::-moz-focus-inner {
-  border: 0;
 }
 .s-main {
   padding: 88px 0 218px 0;
